7e36f5011d30f71586da79e1d2f461bffa329f0d
LE-HU <wojciechlech1337@gmail.com>
2020-12-29 14:59:36 +0100

    hands_up.rb

---
+++ spec/hands_up_spec.rb
@@ -0,0 +1,19 @@
+require_relative("../hands_up.rb")
+
+describe "Hands Up" do
+  it "returns the number of hands raised" do
+    expect(get_positions(54)).to eq([0, 0, 0])
+    expect(get_positions(98)).to eq([2, 2, 1])
+    expect(get_positions(3)).to eq([0, 1, 0])
+  end
+end
+
+
+
+# Kata tests
+
+# Test.describe "Fixed tests" do
+#   Test.assert_equals(get_positions(54), [0, 0, 0])
+#   Test.assert_equals(get_positions(98), [2, 2, 1])
+#   Test.assert_equals(get_positions(3), [0, 1, 0])
+# end
